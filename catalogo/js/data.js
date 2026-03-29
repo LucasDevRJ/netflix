f6ee6aa141bdab7a2d7ed81063addb4394ed4fcd
Lucas <lucaspereiradelima2020@gmail.com>
2026-03-29 15:57:26 -0300

    Adicionando outro projeto.

---
+++ catalogo/js/data.js
@@ -0,0 +1,54 @@
+export const categories = [
+    {
+        title: "Filmes",
+        items: [
+            {
+                img: "https://br.web.img3.acsta.net/pictures/14/10/28/21/28/104479.jpg", top10: true, badge: "Clássico", badgeColor: "red", progress: 0,
+                youtube: "https://www.youtube.com/watch?v=09w9MTtZDEM"
+            },
+            {
+                img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSnpjdxi3kHUI9rTqML8O_7_Ve87-FHFNlLHg&s", progress: 0,
+                youtube: "https://www.youtube.com/watch?v=vKQi3bBA1y8"
+            },
+            {
+                img: "https://isabelaboscov.com/wp-content/uploads/2018/07/blogib_cavaleiro-das-trevas_feat.jpg", progress: 0,
+                youtube: "https://www.youtube.com/watch?v=EXeTwQWrcwY"
+            },
+            {
+                img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSchNtN-awOpsgQTn_SNIoCu7dwOzkT0X89JQ&s", progress: 0,
+                youtube: "https://www.youtube.com/watch?v=vaw_8DoqXfk"
+            },
+            {
+                img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTkSF-HXENyTStN4CD27TSIaaosjzacJizUQQ&s", progress: 0,
+                youtube: "https://www.youtube.com/watch?v=c1nToClX_3w"
+            },
+        ]
+    },
+    {
+        title: "Séries",
+        items: [
+            { img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ2y2JiXMgP8-cDxpExACMDCr1FDYHPy36H4Q&s", top10: true, badge: "Nova temporada", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=_-Nv0JOcGxg&t=12s" },
+            { img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSv8UpOI-9RY7ca7hQHhgB9keR7eK1glynMtg&s", top10: true, youtube: "https://www.youtube.com/watch?v=P_-r11fcDTc" },
+            { img: "https://images.unsplash.com/photo-1505686994434-e3cc5abf1330?q=80&w=600&auto=format&fit=crop", badge: "Novo episódio", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=wLo9bfgla4k" },
+            { img: "https://images.unsplash.com/photo-1505686994434-e3cc5abf1330?q=80&w=600&auto=format&fit=crop", badge: "Novidade", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=wLo9bfgla4k" },
+        ]
+    },
+      {
+        title: "Animes",
+        items: [
+            { img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ9Iy2EZbcFbgKLWP_8tJ9yyERGoNzfToHwHQ&s", top10: true, badge: "Nova temporada", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=22R0j8UKRzY" },
+            { img: "https://m.media-amazon.com/images/S/pv-target-images/457eaab560e12f013b60fcf0627f77fca3938abc3dbd1e2536819e4c6d7cfbef._SX1080_FMjpg_.jpg", top10: true, youtube: "https://www.youtube.com/watch?v=Df_9Dbkog2c" },
+            { img: "https://images.unsplash.com/photo-1505686994434-e3cc5abf1330?q=80&w=600&auto=format&fit=crop", badge: "Novo episódio", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=wLo9bfgla4k" },
+            { img: "https://images.unsplash.com/photo-1505686994434-e3cc5abf1330?q=80&w=600&auto=format&fit=crop", badge: "Novidade", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=wLo9bfgla4k" },
+        ]
+    },
+    {
+        title: "Para maratonar",
+        items: [
+            { img: "https://images.unsplash.com/photo-1574375927938-d5a98e8ffe85?q=80&w=600&auto=format&fit=crop", top10: true, youtube: "https://www.youtube.com/watch?v=wLo9bfgla4k" },
+            { img: "https://images.unsplash.com/photo-1574375927938-d5a98e8ffe85?q=80&w=600&auto=format&fit=crop", top10: true, badge: "Novidade", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=wLo9bfgla4k" },
+            { img: "https://images.unsplash.com/photo-1574375927938-d5a98e8ffe85?q=80&w=600&auto=format&fit=crop", top10: true, badge: "Novo episódio", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=wLo9bfgla4k" },
+            { img: "https://images.unsplash.com/photo-1574375927938-d5a98e8ffe85?q=80&w=600&auto=format&fit=crop", top10: true, badge: "Novo episódio", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=wLo9bfgla4k" },
+        ]
+    }
+];
