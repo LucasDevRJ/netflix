8ef9f317b77e77f8ee540bd868768dd4516f8717
Lucas <lucas.pereira@animati.com.br>
2026-03-30 16:35:29 -0300

    Alterando os conteúdos

--- catalogo/js/data.js
+++ catalogo/js/data.js
@@ -29,8 +29,10 @@ export const categories = [
         items: [
             { img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ2y2JiXMgP8-cDxpExACMDCr1FDYHPy36H4Q&s", top10: true, badge: "Nova temporada", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=_-Nv0JOcGxg&t=12s" },
             { img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSv8UpOI-9RY7ca7hQHhgB9keR7eK1glynMtg&s", top10: true, youtube: "https://www.youtube.com/watch?v=P_-r11fcDTc" },
-            { img: "https://images.unsplash.com/photo-1505686994434-e3cc5abf1330?q=80&w=600&auto=format&fit=crop", badge: "Novo episódio", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=wLo9bfgla4k" },
-            { img: "https://images.unsplash.com/photo-1505686994434-e3cc5abf1330?q=80&w=600&auto=format&fit=crop", badge: "Novidade", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=wLo9bfgla4k" },
+            { img: "https://cincogarotasexemplares.wordpress.com/wp-content/uploads/2016/06/the-mentalist-tv-series.jpg", badge: "Novo episódio", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=nn2Q69pSC_M" },
+            { img: "https://m.media-amazon.com/images/S/pv-target-images/acbc04447a8403b46c707776047f14eff2f261c52f7f7f9e30e0d7c87bd03334.jpg", badge: "Novidade", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=MczMB8nU1sY" },
+            { img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSIOCS5q43wCZwg-84c6o880Ts2ZMtpAELiqQ&s", badge: "Novidade", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=W210J2A49TA" },
+
         ]
     },
       {
@@ -38,8 +40,10 @@ export const categories = [
         items: [
             { img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ9Iy2EZbcFbgKLWP_8tJ9yyERGoNzfToHwHQ&s", top10: true, badge: "Nova temporada", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=22R0j8UKRzY" },
             { img: "https://m.media-amazon.com/images/S/pv-target-images/457eaab560e12f013b60fcf0627f77fca3938abc3dbd1e2536819e4c6d7cfbef._SX1080_FMjpg_.jpg", top10: true, youtube: "https://www.youtube.com/watch?v=Df_9Dbkog2c" },
-            { img: "https://images.unsplash.com/photo-1505686994434-e3cc5abf1330?q=80&w=600&auto=format&fit=crop", badge: "Novo episódio", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=wLo9bfgla4k" },
-            { img: "https://images.unsplash.com/photo-1505686994434-e3cc5abf1330?q=80&w=600&auto=format&fit=crop", badge: "Novidade", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=wLo9bfgla4k" },
+            { img: "https://static.wikia.nocookie.net/liberproeliis/images/7/7c/UYBSml.jpg/revision/latest?cb=20170604183034&path-prefix=pt-br", badge: "Novo episódio", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=YUH1mfV3IEU" },
+            { img: "https://s2-cbn.glbimg.com/crduIMUR_5no6-GsXKggvmGr060=/0x0:1920x1081/888x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_d975fad146a14bbfad9e763717b09688/internal_photos/bs/2026/h/h/wogv4KRjOXIwiw0AYTrw/pokemon.png", badge: "Novidade", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=nS0ico6na_U" },
+            { img: "https://external-preview.redd.it/the-fullmetal-alchemist-2003-anime-is-a-masterpiece-of-v0-Nkr-MkapsAxQOaqPruZSyZ_VdWEdKWzd4TGQRHNfRic.jpg?width=640&crop=smart&auto=webp&s=a8cc18baa6e25ef90f40fda871f5310a16183cd4", badge: "Novidade", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=kx0nBaS_q50" },
+
         ]
     },
     {
